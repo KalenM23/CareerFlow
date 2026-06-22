8370c0278f9c4f560d1f5eacbef070c382de3a9a
Kalen Mclaughlin <62665815+KalenMclaughlin@users.noreply.github.com>
2026-06-22 16:38:08 -0400

    - Made changes to the content view - Added 3 state properties to bind user input - Added text  into the content view to display a string - Added text fields into the content view to capture user input - Added text field styles to change the text field look to roundedborder - Created a enum inside content view as well for the status picker that is going to be created

--- JobTracker/ContentView.swift
+++ JobTracker/ContentView.swift
@@ -10,18 +10,43 @@ import SwiftData
 
 struct ContentView: View {
     
+    // created state properties
+    @State private var jobTitle = ""
+    @State private var companyName = ""
+    @State private var statusSelection: appStatus?
+    
 
     var body: some View {
-       
-        Text("Hello World!")
-            .foregroundStyle(.blue)
-            .font(.title)
-        
+       // created a vstack that have leading alignment and 8 spaces
+        VStack(alignment: .leading, spacing: 8) {
+            // created a text with value job title
+            Text("Job Title")
+            // created a textfield name jobtitle and binded state property to it
+            TextField("Job Title", text: $jobTitle)
+            // change the textfieldstyle to a rounded border
+                .textFieldStyle(.roundedBorder)
+            // created a text with value company name
+            Text("Company Name")
+            // created a text field with a label and binded state var to it
+            TextField("Company Name", text: $companyName)
+            // change the textfieldstyle to a rounded border
+                .textFieldStyle(.roundedBorder)
+                
+                
+            
+            
+        }
+        .padding()
         
         
     }
 
    
+}
+// created a enum for the picker 
+enum appStatus {
+    
+    case Applied, UnderReview, InterviewScheduled, Interviewing, OfferReceived, Accepted, Rejected, Withdrawn
 }
 
 #Preview {
