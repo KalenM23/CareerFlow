d87c806406e5eea2e0727b496c50cc1e9e847146
Kalen Mclaughlin <62665815+KalenMclaughlin@users.noreply.github.com>
2026-06-25 22:03:15 -0400

    - Added changes to the contentview swift file - Added a NavigationStack inside the contentview file - Added a form nested inside the NavigationStack inside the contentviewfile - Deleted the vstack inside the content view file - Moved all the code that was inside the vstack into the form inside the navigationstack - Removed the padding at the end of the vstack as well

--- JobTracker/ContentView.swift
+++ JobTracker/ContentView.swift
@@ -20,66 +20,62 @@ struct ContentView: View {
 
     var body: some View {
         
-       // created a vstack that have leading alignment and 8 spaces
-        VStack(alignment: .leading, spacing: 8) {
-            // created a text with value job title
-            Text("Job Title")
-            // created a textfield name jobtitle and binded state property to it
-            TextField("Job Title", text: $jobTitle)
-            // change the textfieldstyle to a rounded border
-                .textFieldStyle(.roundedBorder)
-            // created a text with value company name
-            Text("Company Name")
-            // created a text field with a label and binded state var to it
-            TextField("Company Name", text: $companyName)
-            // change the textfieldstyle to a rounded border
-                .textFieldStyle(.roundedBorder)
+        NavigationStack {
+            Form {
                 
-            // List with a picker that allow users to show their applications status
-            
-            List {
-                Picker("Status", selection: $statusSelection) {
-                    Text("Applied").tag(appStatus.Applied)
-                    Text("UnderReview").tag(appStatus.UnderReview)
-                    Text("Interview Scheduled").tag(appStatus.InterviewScheduled)
-                    Text("Interviewing").tag(appStatus.Interviewing)
-                    Text("Offer Received").tag(appStatus.OfferReceived)
-                    Text("Accepted").tag(appStatus.Accepted)
-                    Text("Rejected").tag(appStatus.Rejected)
-                    Text("Withdrawn").tag(appStatus.Withdrawn)
+                Section("Job Application") {
+                    // created a text with value job title
+                    Text("Job Title")
+                    // created a textfield name jobtitle and binded state property to it
+                    TextField("", text: $jobTitle)
+                    // change the textfieldstyle to a rounded border
+                        .textFieldStyle(.roundedBorder)
+                    // created a text with value company name
+                    Text("Company Name")
+                    // created a text field with a label and binded state var to it
+                    TextField("", text: $companyName)
+                    // change the textfieldstyle to a rounded border
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Picker("Status", selection: $statusSelection) {
+                        Text("Applied").tag(appStatus.Applied)
+                        Text("UnderReview").tag(appStatus.UnderReview)
+                        Text("Interview Scheduled").tag(appStatus.InterviewScheduled)
+                        Text("Interviewing").tag(appStatus.Interviewing)
+                        Text("Offer Received").tag(appStatus.OfferReceived)
+                        Text("Accepted").tag(appStatus.Accepted)
+                        Text("Rejected").tag(appStatus.Rejected)
+                        Text("Withdrawn").tag(appStatus.Withdrawn)
+                    }
                         
+                    // Date picker that allow users to click their application date and time
+                    DatePicker("Date", selection: $applicationDate)
+                    
+                    Button {
+                        // Action goes here
+                        // what does the button do
+                    } label: {
+                        Text("Add Application")
+                            .frame(maxWidth: 350)
+                            
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle)
+                    
+                    
                 }
-            }
-            // Date picker that allow users to click their application date and time
-            DatePicker("Application Date", selection: $applicationDate)
-            
-            Button {
-                // Action goes here
-                // what does the button do
-            } label: {
-                Text("Add Application")
-                    .frame(maxWidth: 350)
                     
             }
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.roundedRectangle)
-           
-
-            
-           
-            
-            
-                
-            
-            
-        }
-        .padding()
+       }
         
-        
+            
     }
+        
+        
+}
 
    
-}
+
 // created a enum for the picker
 enum appStatus {
     
